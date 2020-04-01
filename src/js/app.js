b4b08044837182e0ffd009bc291e14abc52cbf73
bohdan-yelk <bohdandidenko@yelk.com.ua>
2020-04-01 19:54:46 +0300

    follow, learn,popular,reviews sections, footer

--- src/js/app.js
+++ src/js/app.js
@@ -23,57 +23,13 @@ import { log } from './utils'
     dots: true
   })
 
-  // advantages section number count
-
-    // var show = true
-    // $(window).on("scroll load resize", function(){
-    //   if(!show) return false
-
-    //   let w_top = $(window).scrollTop(),
-    //       e_top = $('#counter').offset().top,
-    //       w_height = $(window).height(),
-    //       d_height = $(document).height(),
-    //       e_height = $('#counter').outerHeight()
-
-    //       console.log(w_top + 200 + '' + e_top)
-
-    //       if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top , w_height) {
-    //         $('.spincrement')
-    //       }
-    // })
-
-    // var a = 0;
-    // $(window).scroll(function() {
-
-    // var oTop = $('#counter').offset().top - window.innerHeight;
-    // if (a == 0 && $(window).scrollTop() > oTop) {
-    //   $('.counter-value').each(function() {
-    //     var $this = $(this),
-    //       countTo = $this.attr('data-count');
-    //     $({
-    //       countNum: $this.text()
-    //     }).animate({
-    //         countNum: countTo
-    //       },
-
-    //       {
-
-    //         duration: 2000,
-    //         easing: 'swing',
-    //         step: function() {
-    //           $this.text(Math.floor(this.countNum))
-    //         },
-    //         complete: function() {
-    //           $this.text(this.countNum)
-    //           //alert('finished');
-    //         }
-
-    //       })
-    //   });
-    //   a = 1
-    // }
-
-    // })
+  $('#reviewsSlider').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true
+  })
 
   /* --------  Apply global listeners  -------- */
 })(window.jQuery)
